5c43a5f9aef04b27c29e072ad57096df60ac7197
Blake <dblakev@gmail.com>
2017-04-28 15:51:18 -0700

    from Rstudio

---
+++ foo.r
@@ -0,0 +1,15 @@
+#---
+#title: "Happy Git with R"
+#author: "Blake Valenta"
+#date: "2017-03-06"
+#output: 
+#  html_document: 
+#    keep_md: yes
+#---
+
+#+ setup, include = FALSE
+c <-  c("a","b","c")
+print(c)
+six <- 2+4
+
+
